af71ff4140ac085ce52421c7d774a13643ffd137
HyperCrowd <github@hypercrowd.org>
2022-07-02 02:28:07 -0500

    1 file modified

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
 import { Command } from 'commander';
+
+/**
+ * Get tweets piped in from a CURL query
+ * Split words and strip stop words
+ * Extract domains
+ * Extact times
+ * Extract username
+ * INSERT username TO users IF NOT UNIQUE
+ * INSERT tweetId, users.id, timestamp TO tweetIds IF NOT UNIQUE
+ * INSERT word, tweetId.id TO words
+ *   IF (word, tweetId.id) EXISTS
+ *     iterate counter by 1
+ *   ELSE
+ *     set counter to 1
+ * INSERT domain, tweetId.id TO domains
+ *   IF (domain, tweetId.id) EXISTS
+ *     iterate counter by 1
+ *   ELSE
+ *     set counter to 1
+ * Run queries to generate report
+ * Create a report image
+ */
